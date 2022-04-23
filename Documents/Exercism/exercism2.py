5e0bb53a0dd99e67aec7ac76962eecc537ec42cf
Sergio Gomez <seluigo@hotmail.com>
2022-04-23 17:20:45 -0500

    third commit

---
+++ Documents/Exercism/exercism2.py
@@ -0,0 +1,9 @@
+#Given a moment, determine the moment that would be after a gigasecond has passed.
+
+#A gigasecond is 10^9 (1,000,000,000) seconds.
+
+import datetime
+def add(moment):
+    return moment + datetime.timedelta(seconds=10**9)
+
+
